a143aab5ca1d46c59a5f854f46cfe70dd694d917
Sytabram <bryan.zweiacker@sytabram.ch>
2025-09-03 11:28:56 +0200

    Feat: Add Dispatcher to index

--- Public/index.php
+++ Public/index.php
@@ -1,17 +1,13 @@
 <?php
+require '../src/dispatcher.php';
 class Index {
+
+    function __construct() {
+        $dispatcher = new Dispatcher();
+        $dispatcher->dispatch();
+    }
     public function render($viewFile) {
         $viewPath = '../src/views/' . $viewFile;
-        if (file_exists($viewPath)) {
-            require $viewPath;
-        } else {
-            // Envoyer le code d'erreur 404
-            header("HTTP/1.1 404 Not Found");
-            echo "Erreur 404 : La page demandée n'existe pas.";
-        }
+        require $viewPath;
     }
 }
-$page = new Index();
-
-$page->render('home2.php');
-
