00acc082afc42cd16ea6f450e90f04a54f0cc75a
Amin <pq67lmc@eduvaud.ch>
2025-09-03 10:27:44 +0200

    Feat: renderer a file

--- Src/Views/home.php
+++ Src/Views/home.php
@@ -1,1 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+    <meta charset='UTF-8'>
+    <title>Homepage</title>
+</head>
+<body>
+<h1>page d'acceuil teste</h1>
+<p>teste</p>
+</body>
+</html>
