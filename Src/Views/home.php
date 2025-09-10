c1c45e4638fe16fb8178985a6eae111d4e38dbc7
Sytabram <bryan.zweiacker@sytabram.ch>
2025-09-10 11:39:57 +0200

    Fix: Change path of css and assets

--- Src/Views/home.php
+++ Src/Views/home.php
@@ -7,13 +7,13 @@
     <meta name="csrf-param" content="authenticity_token" />
 <meta name="csrf-token" content="/PyM5hNxoFNfo6rYHV9HHLwEUvK+VZ9Pra4AMCk5ClnkQ5DTd0lCHhAVuX1dhs3mPrffleIGViC/6ZCQwN+Cgg==" />
     
-    <link rel="stylesheet" media="all" href="home.css" />
+    <link rel="stylesheet" media="all" href="css/home.css" />
   </head>
 
   <body>
     <header class="dashboard">
   <section class="container">
-    <p><img src="../assets/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png" /></p>
+    <p><img src="assets/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png" /></p>
     <h1>Exercise<br>Looper</h1>
   </section>
 </header>
