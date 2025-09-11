3a776829819ab8c4edb452b8a60d9b78989090bd
Amin <pq67lmc@eduvaud.ch>
2025-09-11 16:20:34 +0200

    add: db,controller navigate

---
+++ Src/Models/database.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $db   = 'Maw1.1Looper';
+    private $user = 'root';
+    private $pass = 'Pa$$w0rd';
+    private $charset = 'utf8mb4';
+
+    private $pdo;
+
+    public function __construct() {
+        $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+        try {
+            $this->pdo = new PDO($dsn, $this->user, $this->pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $e) {
+            echo "Erreur de connexion : " . $e->getMessage();
+            exit;
+        }
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
