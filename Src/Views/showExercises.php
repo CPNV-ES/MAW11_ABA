52ded24c36cc87f25c6389ba9c15836cc965d73f
Amin <pq67lmc@eduvaud.ch>
2025-09-30 11:31:52 +0200

    add : html of exercises link with css

--- Src/Views/showExercises.php
+++ Src/Views/showExercises.php
@@ -1,13 +1,29 @@
-<?php if (!empty($exercises)): ?>
-    <h2>Liste des exercices</h2>
-    <ul>
-        <?php foreach ($exercises as $exercise): ?>
-            <li>
-                ID: <?= htmlspecialchars($exercise['id']) ?> -
-                Titre: <?= htmlspecialchars($exercise['titre']) ?>
-            </li>
-        <?php endforeach; ?>
+
+<header class="heading answering">
+    <section class="container">
+        <a href="/"><img src="/assets/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png" /></a>
+
+    </section>
+</header>
+
+<main class="container">
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>ExerciseLooper</title>
+        <meta name="csrf-param" content="authenticity_token" />
+        <meta name="csrf-token" content="BXsG3Y5pcBftAp2Cra3Q6bQm828bQsdKutU1Pxa1PPSV3nsf8/34NTbrwbUyvOgQI74jNFBr4h4lCu4xOngsfA==" />
+
+
+        <link rel="stylesheet" media="all" href="/css/home.css" />
+    </head>
+
+    <body>
+    <ul class="ansering-list">
+       
     </ul>
-<?php else: ?>
-    <p>Aucun exercice trouvé.</p>
-<?php endif; ?>
+
+    </body>
+    </html>
+
+</main>
