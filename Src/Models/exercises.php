66fcb3bb5f3c4c194a08394f93c03e94314cbde6
Amin <pq67lmc@eduvaud.ch>
2025-10-08 09:24:23 +0200

    fix : change sql request , the database has changed

--- Src/Models/exercises.php
+++ Src/Models/exercises.php
@@ -10,7 +10,7 @@ class Exercise {
     }
 
     public function getExercises() {
-        $stmt = $this->pdo->query("SELECT id, title FROM `exercise`");
+        $stmt = $this->pdo->query("SELECT exercise_id, title FROM `exercises`");
         return $stmt->fetchAll();
     }
 }
