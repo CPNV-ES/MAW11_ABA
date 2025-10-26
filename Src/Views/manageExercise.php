5ab022b87484b2cdcb4dd23456165b8d7e36b24f
Bryan Zweiacker <bryan.zweiacker@sytabram.ch>
2025-10-26 18:33:07 +0100

    feat: add redirection to the button manage fields view

--- Src/Views/manageExercise.php
+++ Src/Views/manageExercise.php
@@ -34,7 +34,7 @@
                     <tr>
                         <td><?= $exercise["title"]?></td>
                         <td>
-                            <a title="Manage fields" href="#"><img src="/img/edit.png" class="fa fa-trash"></a>
+                            <a title="Manage fields" href="/exercises/<?= $exercise["exercise_id"] ?>/fields"><img src="/img/edit.png" class="fa fa-trash"></a>
                             <a title="delete" href="#" onclick="if(confirm('Are you sure?')){document.getElementById('delete-form-<?= $exercise["exercise_id"] ?>').submit();} return false;"><img src="/img/trash.png" class="fa fa-trash"></a>
                             <form id="delete-form-<?= $exercise["exercise_id"] ?>" method="POST" action="/exercises/delete" style="display:none;">
                                 <input type="hidden" name="exercise_id" value="<?= $exercise["exercise_id"] ?>">
@@ -85,7 +85,7 @@
                     <tr>
                         <td><?= $exercise["title"]?></td>
                         <td>
-                            <a title="Manage fields" href="#"><img src="/img/stats.png" class="fa fa-edit"></a>
+                            <a title="Manage fields" href="/exercises/<?= $exercise["exercise_id"] ?>/fields"><img src="/img/stats.png" class="fa fa-edit"></a>
                             <a title="delete" href="#" onclick="if(confirm('Are you sure?')){document.getElementById('delete-form-closed-<?= $exercise["exercise_id"] ?>').submit();} return false;"><img src="/img/trash.png" class="fa fa-trash"></a>
                             <form id="delete-form-closed-<?= $exercise["exercise_id"] ?>" method="POST" action="/exercises/delete" style="display:none;">
                                 <input type="hidden" name="exercise_id" value="<?= $exercise["exercise_id"] ?>">
