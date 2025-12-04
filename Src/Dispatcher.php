c0171cb8444be1dd91c3e296abd63b15cd01d9fe
Bryan Zweiacker <bryan.zweiacker@sytabram.ch>
2025-12-04 15:21:42 +0100

    feat: add POST handling and form validation for exercise fields

--- Src/Dispatcher.php
+++ Src/Dispatcher.php
@@ -17,6 +17,8 @@ Class Dispatcher{
 
             if ($method == 'GET') {
                 $fieldController->manageFields($exerciseId);
+            } else if ($method == 'POST') {
+                $fieldController->createField($exerciseId);
             }
 
             return;
