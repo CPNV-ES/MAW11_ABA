02c9a73b00007fc1068913dbf08ac79dc9c2fc44
Aurélien <aurelien.a.robert@gmail.com>
2025-12-10 15:39:16 +0100

    feat: delete fields from button

--- Src/Controllers/Fields.php
+++ Src/Controllers/Fields.php
@@ -124,4 +124,12 @@ class Fields
             'data' => $data,
         ]);
     }
+
+        public function destroy($exerciseId,$fieldId)
+    {
+        $this->fieldModel->destroy($fieldId);
+     
+         header("Location: /exercises/$exerciseId/fields");
+        exit;
+    }
 }
