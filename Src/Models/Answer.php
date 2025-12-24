9b65cd460dd4e4192b26b20e9a758e1e1a203753
Bryan Zweiacker <bryan.zweiacker@sytabram.ch>
2025-12-24 17:27:41 +0100

    refactor: Answer class for PSR-12 compliance

--- Src/Models/Answer.php
+++ Src/Models/Answer.php
@@ -1,7 +1,9 @@
 <?php
+
 require_once 'Database.php';
 
-class Answer {
+class Answer
+{
     private $pdo;
 
     public function __construct(Database $db)
@@ -9,7 +11,8 @@ class Answer {
         $this->pdo = $db->getConnection();
     }
 
-    function createFulfillment($exercise_id) {
+    public function createFulfillment($exercise_id)
+    {
         date_default_timezone_set('Europe/Zurich');
         $stmt = $this->pdo->prepare("INSERT INTO fulfillments (exercise_id, fulfillment_date) VALUES (:exercise_id, :fulfillment_date)");
         $stmt->execute([
@@ -20,7 +23,8 @@ class Answer {
         return $this->pdo->lastInsertId();
     }
 
-    function insert($fulfillment_id, $field_id){
+    public function insert($fulfillment_id, $field_id)
+    {
         date_default_timezone_set('Europe/Zurich');
         $stmt = $this->pdo->prepare("INSERT INTO answers (fulfillment_id, field_id, answer_text) VALUES (:fulfillment_id, :field_id, :answer_text)");
         $stmt->execute([
@@ -32,7 +36,8 @@ class Answer {
         return $this->pdo->lastInsertId();
     }
 
-    function updateById($answer, $answer_id){
+    public function updateById($answer, $answer_id)
+    {
         $stmt = $this->pdo->prepare("UPDATE answers SET answer_text = :answer_text WHERE answer_id = :answer_id");
         $stmt->execute([
             'answer_text' => $answer,
@@ -40,10 +45,10 @@ class Answer {
         ]);
     }
 
-    function getByFulfillmentId($fulfillment_id) {
+    public function getByFulfillmentId($fulfillment_id)
+    {
         $stmt = $this->pdo->prepare("SELECT * FROM answers WHERE fulfillment_id = :fulfillment_id ORDER BY answer_id");
         $stmt->execute(['fulfillment_id' => $fulfillment_id]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
 }
